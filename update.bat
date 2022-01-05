f0d40d8009f55b89367977cb1b2c38b051ba21e0
won0-kim <won10.kim@gmail.com>
2022-01-06 00:30:43 +0900

    remake 1

--- update.bat
+++ update.bat
@@ -1,4 +1,11 @@
 @echo off
+
+if "%~1"=="" (
+    git reset --hard origin/main
+) else (
+    git reset --hard origin/%1%
+)
+
 if exist requirements.txt (
     python -m pip install -r requirements.txt
 )
